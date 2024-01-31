e45b4c43ad4c241dc37c956b15120df40dc721c8
Ravi Vishwakarma <ravi_vishwakarma1@persistent.com>
2024-01-31 13:25:12 +0530

    first commit

---
+++ src/test/java/org/yourcompany/project/CalculatorTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+     @Test
+     public void evaluatesExpression() {
+        Calculator calculator = new Calculator();
+        int sum1 = calculator.evaluate("1+2+3");
+        int sum2 = calculator.evaluate("1+2+4");
+        assertEquals(6, sum1);
+        assertEquals(7, sum2);
+
+     }
+}
