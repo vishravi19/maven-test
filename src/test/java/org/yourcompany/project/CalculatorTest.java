c1714c235d1c107b21b277f4d7a77634fc32b17c
Ravi Nayak Vishwakarma <91801541+vishravi19@users.noreply.github.com>
2024-02-19 17:39:37 +0530

    Update CalculatorTest.java

--- src/test/java/org/yourcompany/project/CalculatorTest.java
+++ src/test/java/org/yourcompany/project/CalculatorTest.java
@@ -6,7 +6,7 @@ public class CalculatorTest {
      public void evaluatesExpression() {
         Calculator calculator = new Calculator();
         int sum1 = calculator.evaluate("1+2+3");
-        int sum2 = calculator.evaluate("1+2+4");
+        int sum2 = calculator.evaluate("1+2+5");
         assertEquals(6, sum1);
         assertEquals(7, sum2);
 
